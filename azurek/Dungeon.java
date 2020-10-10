db4a65da72a29f2a5a17f6a4357472e06700f313
adamzurek1066 <azurek@comcast.net>
2020-10-10 16:58:59 -0400

    Dungeon Skeleton

---
+++ azurek/Dungeon.java
@@ -0,0 +1,23 @@
+public class Dungeon{
+
+    void getDungeon(String name, int width, int gameHeight){
+        System.out.println("getDungeon");
+    }
+
+    void addRoom(Room r){
+        System.out.println("addRoom");
+    }
+
+    void addCreature(Creature c){
+        System.out.println("addCreature");
+    }
+
+    void addPassage(Passage passage){
+        System.out.println("addPassage");
+    }
+
+    void addItem(Item item){
+        System.out.println("addItem");
+    }
+
+}
