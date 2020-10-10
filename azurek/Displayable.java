9963e1c99fc9ca36973e2b7fcc92650d938a2c8a
adamzurek1066 <azurek@comcast.net>
2020-10-10 17:15:37 -0400

    Displayable

---
+++ azurek/Displayable.java
@@ -0,0 +1,45 @@
+public class Displayable{
+    void setInvisible(){
+        System.out.println("setInvisible");
+    }
+
+    void setVisible(){
+        System.out.println("setVisible");
+    }
+
+    void setMaxHit(int maxHit){
+        System.out.println("setMaxHit");
+    }
+
+    void setHpMove(int hpMoves){
+        System.out.println("setHpMove");
+    }
+
+    void setHp(int Hp){
+        System.out.println("setHp");
+    }
+
+    void setType(char t){
+        System.out.println("setType");
+    }
+
+    void setIntValue(int v){
+        System.out.println("setIntValue")
+    }
+
+    void SetPosX(int x){
+        System.out.println("SetPosX");
+    }
+
+    void setPosY(int y){
+        System.out.println("setPosY");
+    }
+
+    void SetWidth(int x){
+        System.out.println("SetWidth");
+    }
+
+    void setHeight(int y){
+        System.out.println("setHeight");
+    }
+}
