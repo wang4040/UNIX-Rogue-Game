02cf8168533293415bf496cddba85b23d143a1f8
wang4040 <jerry2000wang@yahoo.ca>
2020-11-24 22:58:44 -0500

    blesscurse

--- azurek/src/game/ItemAction.java
+++ azurek/src/game/ItemAction.java
@@ -14,6 +14,26 @@ public class ItemAction extends Action{
 
     void BlessCurseOwner(Creature owner){
         //System.out.println("ItemAction BlessCurseOwner " + owner);
+		String message;
+		if(charValue == 'a') {
+			if(owner.getArmor() != null) {
+				int newVal = owner.getArmor().getIntValue;
+				newVal += intValue;
+				owner.getArmor().setIntValue(newVal);
+				message = owner.getArmor().getName() + " cursed! " + intVal + " taken from its effectiveness"
+			}else {
+				message = "scroll of cursing does nothing because armor is not being used"
+		}else if(charValue == 'w') {
+			if(owner.getWeapon() != null) {
+				int newVal = owner.getWeapon().getIntValue;
+				newVal += intValue;
+				owner.getWeapon().setIntValue(newVal);
+				message = owner.getWeapon().getName() + " cursed! " + intVal + " taken from its effectiveness"
+			}else {
+				message = "scroll of cursing does nothing because weapon is not being used"
+			}
+		}
+		Rogue.displayMessageInfo(message);
     }
 
     void Hallucinate(Creature owner){
